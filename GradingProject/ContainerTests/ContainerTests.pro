d693b4dd84c78ba4686f15008058864b32e0f6ed
bennettc <bennettc@lafayette.edu>
2017-04-08 16:11:21 -0400

    adding testing functionality to project

--- GradingProject/ContainerTests/ContainerTests.pro
+++ GradingProject/ContainerTests/ContainerTests.pro
@@ -3,4 +3,35 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+  LIBS += -pthread
+
+  SOURCES += main.cpp
+
+  SOURCES += ../gtest/googletest/src/gtest-all.cc
+
+  INCLUDEPATH +=  ../gtest                                   \
+                  ../gtest/googletest                        \
+                  ../gtest/googletest/include                \
+                  ../gtest/googletest/include/gtest          \
+                  ../gtest/googletest/include/gtest/gtest.h
+
+HEADERS += ../DataContainers/comment.h
+SOURCES += ../DataContainers/comment.cpp
+
+HEADERS += ../DataContainers/lab.h
+SOURCES += ../DataContainers/lab.cpp
+
+HEADERS += ../DataContainers/labassignment.h
+SOURCES += ../DataContainers/labassignment.cpp
+
+HEADERS += ../DataContainers/rubricitem.h
+SOURCES += ../DataContainers/rubricitem.cpp
+
+HEADERS += ../DataContainers/section.h
+SOURCES += ../DataContainers/section.cpp
+
+HEADERS += ../DataContainers/student.h
+SOURCES += ../DataContainers/student.cpp
+
+HEADERS += ../DataContainers/template.h
+SOURCES += ../DataContainers/template.cpp
--- GradingProject/ContainerTests/main.cpp
+++ GradingProject/ContainerTests/main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <iostream>
+#include <string>
+#include "gtest/gtest.h"
+#include "../DataContainers/comment.h"
+#include "../DataContainers/lab.h"
+#include "../DataContainers/labassignment.h"
+#include "../DataContainers/rubricitem.h"
+#include "../DataContainers/section.h"
+#include "../DataContainers/student.h"
+#include "../DataContainers/template.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+
+
+class ReferanceTest : public ::testing::Test {
+
+protected:
+
+
+    ReferanceTest()
+    {
+
+    }
+
+    virtual ~ReferanceTest()
+    {
+
+    }
+
+    virtual void SetUp()
+    {
+
+    }
+
+    virtual void TearDown()
+    {
+
+    }
+
+};
+
+TEST(general, COMMENT_TEST)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    Comment c1("new comment");
+    ASSERT_EQ(c1.get_Comment(), "new comment") << "special comment constructor stores comment string";
+    c1.set_line(24);
+    ASSERT_EQ(c1.get_line(), 24);
+    RubricItem * temp = new RubricItem("test", 10);
+    c1.set_Rubric_Item(temp);
+    ASSERT_EQ(c1.get_Rubric_Item(), temp);
+    ASSERT_EQ(c1.get_Rubric_Item()->get_Subject(), "test");
+    ASSERT_EQ(c1.get_Rubric_Item()->get_Points(), 10);
+}
+
+
+int main(int argc, char **argv) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
