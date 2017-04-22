5cd365c19a39140f90427ed326047cccd4a28303
talwarn <talwarn@lafayette.edu>
2017-04-22 17:01:29 -0400

    added method headers in DBEngine

--- GradingProject/DBEngine/dbengine.h
+++ GradingProject/DBEngine/dbengine.h
@@ -10,6 +10,14 @@ class DBEngine{
 public:
     DBEngine(DBTool * dbtool);
     ~DBEngine();
+    void store_section(int id);
+    void store_student(int id, std::string name);
+    void store_lab(int id, int sectionId, int labNum, int templateId);
+    void store_template(int id);
+    void store_labAssignment(int id, int grade, int studentId, int labId);
+    void store_rubricItem(int id, std::string subjects, int points);
+    void store_comment(int id, std::string comment, int lineNum, std::string filename, int rubricItemId);
+
 private:
     DBTool * dbtool;
 };
